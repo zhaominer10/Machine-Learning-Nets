fe18af7067badd5505a4d6878d560f93e5a73e15
zhaominer10 <zhaominer10@gmail.com>
2022-05-25 19:07:22 +0800

    update VGG of tensorflow version

--- notes.md
+++ notes.md
@@ -1,3 +1,3 @@
 - [*args & **kwargs](https://www.programiz.com/python-programming/args-and-kwargs#:~:text=*args%20and%20**kwargs%20are,the%20tuple%20can%20be%20performed.)
 - [layer weight initializers](https://keras.io/api/layers/initializers/#variancescaling-class) in keras
-- 
+- [batch normalization](https://towardsdatascience.com/batch-normalization-in-3-levels-of-understanding-14c2da90a338)
---
+++ tensorflow/VGG/predict.py
@@ -0,0 +1,53 @@
+import os
+import json
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from model import vgg
+
+
+def main():
+    im_height = 224
+    im_width = 224
+    num_classes = 5
+
+    img_path = '../../dataset/predict_image/sunflower.jpg'
+    assert os.path.exists(img_path), 'file: {} does not exist.'.format(img_path)
+    img = Image.open(img_path).convert('RGB')
+
+    img = img.resize((im_width, im_height))
+    plt.imshow(img)
+    plt.show()
+
+    img = np.array(img) / 255.
+
+    # add the image to a batch where it's the only member
+    img = (np.expand_dims(img, 0))
+
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: {} does not exist.".format(json_path)
+    with open(json_path, 'r') as f:
+        class_indices = json.load(f)
+
+    model = vgg(model_name='vgg16', im_height=im_height, im_width=im_width, num_classes=num_classes)
+    weights_path = './weights/myVGG.h5'
+    assert os.path.exists(weights_path), "file {} does not exist.".format(weights_path)
+    model.load_weights(weights_path)
+
+    result = np.squeeze(model.predict(img))
+    print(result)
+    predict_class = np.argmax(result)
+    print(predict_class)
+
+    print_res = "predicted class is {} which has prob: {:.3}".format(class_indices[ str(predict_class) ],
+                                                                     result[ predict_class ])
+
+    print(print_res)
+    print()
+    for i in range(len(result)):
+        print('class: {:10} prob: {:.3}'.format(class_indices[ str(i) ],
+                                                result[ i ]))
+
+
+if __name__ == '__main__':
+    main()
