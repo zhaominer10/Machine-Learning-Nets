b6167e4304e6c299b4a335734df5838962dad4d2
zhaominer10 <zhaominer10@gmail.com>
2022-05-29 23:18:37 +0800

    weekend upate 20220529

--- tensorflow/GoogLeNet/model.py
+++ tensorflow/GoogLeNet/model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras import layers, models, Models, Sequential
+from keras import layers, models, Models, Sequential
 
 
 def GoogLeNet(im_height=224, im_width=224, num_classes=1000, aux_logits=False):
@@ -8,4 +8,11 @@ def GoogLeNet(im_height=224, im_width=224, num_classes=1000, aux_logits=False):
 class Inception(layers.Layer):
     def __init__(self, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, poo_proj, **kwargs):
         super(Inception, self).__init__(**kwargs)
+        self.branch1 = layers.Conv2D(ch1x1, kernel_size=1, activation='relu')
+
+        self.branch2 = Sequential([
+            layers.Conv2D(ch3x3red, kernel_size=1, activation='relu'),
+            layers.Conv2D(ch3x3, kernel_size=3, padding='SAME', activation='relu')
+        ])
+
 
