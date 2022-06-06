2758a4fd7a1533f828fdf0e407269d60d49d4539
zhaominer10 <zhaominer10@gmail.com>
2022-06-06 17:57:10 +0800

    update resnet 20220606

--- pytorch/ResNet/model.py
+++ pytorch/ResNet/model.py
@@ -5,7 +5,7 @@ import torch
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __int__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channel,
                                out_channels=out_channel,
@@ -66,11 +66,11 @@ class Bottleneck(nn.Module):
                                bias=False)
         self.bn2 = nn.BatchNorm2d(out_channel)
         self.conv3 = nn.Conv2d(in_channels=out_channel,
-                               out_channels=out_channel*self.expansion,
+                               out_channels=out_channel * self.expansion,
                                kernel_size=1,
                                stride=1,
                                bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
 
@@ -110,9 +110,19 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(self.in_channel)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer1 = self._make_layer(block, 64, blocks_num[ 0 ])
         self.layer2 = self._make_layer(block, 128, blocks_num[ 1 ], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[ 2 ], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks_num[ 3 ], stride=2)
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    # 非常巧妙
     def _make_layer(self, block, channel, block_num, stride=1):
         downsample = None
         if stride != 1 or self.in_channel != channel * block.expansion:
@@ -121,11 +131,8 @@ class ResNet(nn.Module):
                 nn.BatchNorm2d(channel * block.expansion)
             )
 
-        layers = []
-        layers.append(block(self.in_channel,
-                            channel,
-                            downsample=downsample,
-                            stride=stride))
+        layers = [ ]
+        layers.append(block(self.in_channel, channel, downsample=downsample, stride=stride))
         self.in_channel = channel * block.expansion
 
         for _ in range(1, block_num):
@@ -133,25 +140,32 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+
+        return x
 
 
+def resnet34(num_classes=1000, include_top=True):
+    return ResNet(BasicBlock, [ 3, 4, 6, 3 ], num_classes=num_classes, include_top=include_top)
 
 
+def resnet50(num_classes=1000, include_top=True):
+    return ResNet(BasicBlock, [ 3, 4, 6, 3 ], num_classes=num_classes, include_top=include_top)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def resnet101(num_classes=1000, include_top=True):
+    return ResNet(Bottleneck, [ 3, 4, 23, 3 ], num_classes=num_classes, include_top=include_top)
