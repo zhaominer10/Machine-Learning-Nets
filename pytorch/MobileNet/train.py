344c5305ddf0a4b6198a4d056effa64fde14afad
zhaominer10 <zhaominer10@gmail.com>
2022-06-20 16:20:29 +0800

    add mobile net

---
+++ pytorch/MobileNet/train.py
@@ -0,0 +1,133 @@
+import os
+import sys
+import json
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from tqdm import tqdm
+
+from model_v2 import MobileNetV2
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('using {} device.'.format(device))
+
+batch_size = 32
+epochs = 5
+
+data_transform = {
+    "train": transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+
+    "val": transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+image_path = os.path.join(data_root, "dataset", "flower_data")
+assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+
+train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                     transform=data_transform["train"])
+val_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                   transform=data_transform["val"])
+
+train_num = len(train_dataset)
+val_num = len(val_dataset)
+
+flower_list = train_dataset.class_to_idx
+cla_dict = dict((val, key) for key, val in flower_list.items())
+json_str = json.dumps(cla_dict, indent=4)
+with open("class_indices.json", "w") as json_file:
+    json_file.write(json_str)
+
+nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+print('using {} dataloader workers every process.'.format(nw))
+
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True,
+                                           num_workers=nw)
+val_loader = torch.utils.data.DataLoader(val_dataset,
+                                         batch_size=batch_size,
+                                         shuffle=False,
+                                         num_workers=nw)
+
+print("using {} images for training, {} images for validation.".format(train_num, val_num))
+
+net = MobileNetV2(num_classes=5)
+model_weight_path = "./mobilenet_v2.pth"
+assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+pre_weights = torch.load(model_weight_path, map_location='cpu')
+
+# for k, v in pre_weights.items():
+#     print(k)
+
+# delete last layer (classifier layer) weights
+pre_dict = {k: v for k, v in pre_weights.items() if "classifier" not in k}
+missing_keys, unexpected_keys = net.load_state_dict(pre_dict, strict=False)
+
+for param in net.features.parameters():
+    param.requires_grad = False
+
+net.to(device)
+
+loss_function = nn.CrossEntropyLoss()
+params = [p for p in net.parameters() if p.requires_grad]
+optimizer = optim.Adam(params, lr=0.0001)
+
+best_acc = 0.0
+save_path = './mobilenet_v2_self.pth'
+train_steps = len(train_loader)
+
+for epoch in epochs:
+    net.train()
+    running_loss = 0.0
+    train_bar = tqdm(train_loader, file=sys.stdout)
+    for step, data in enumerate(train_bar):
+        images, labels = data
+        optimizer.zero_grad()
+        logits = net(images.to(device))
+        loss = loss_function(logits, labels.to(device))
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        train_bar.desc = 'train epoch[{}/{}] loss:{:.3f}'.format(epoch + 1,
+                                                                 epochs,
+                                                                 loss)
+
+    net.eval()
+    acc = 0.0
+    with torch.no_grad():
+        val_bar = tqdm(val_loader, file=sys.stdout)
+        for val_data in val_bar:
+            val_image, val_labels = val_data
+            outputs = net(val_image.to(device))
+            predict_y = torch.max(outputs, dim=1)[1]
+            acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+            val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+                                                       epochs)
+
+    val_accurate = acc / val_num
+    print("[epoch %d] train_loss: %.3f  val_accuracy: %.3f" %
+          (epoch + 1, running_loss / train_steps, val_accurate))
+
+    if val_accurate > best_acc:
+        best_acc = val_accurate
+        torch.save(net.state_dict(), save_path)
+
+print('finished training')
+
+
+
