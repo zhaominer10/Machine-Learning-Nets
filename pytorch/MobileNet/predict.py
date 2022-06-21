412012601556587a777dd79b16278ed8f10e2dd8
zhaominer10 <zhaominer10@gmail.com>
2022-06-21 15:25:16 +0800

    update

--- pytorch/MobileNet/predict.py
+++ pytorch/MobileNet/predict.py
@@ -7,22 +7,52 @@ import matplotlib.pyplot as plt
 
 from model_v2 import MobileNetV2
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-data_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+def main():
 
-img_path = "../../dataset/predict_image/sunflower.jpg"
-assert os.path.exists(img_path), "file: {} does not exists.".format(img_path)
-img = Image.open(img_path)
-plt.imshow(img)
-# plt.show()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-img = data_transform(img)
-img = torch.unsqueeze(img, dim=0)
+    data_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-json_path = './'
+    img_path = "../../dataset/predict_image/sunflower.jpg"
+    assert os.path.exists(img_path), "file: {} does not exists.".format(img_path)
+    img = Image.open(img_path).convert("RGB")
+    plt.imshow(img)
+    # plt.show()
+
+    img = data_transform(img)
+    img = torch.unsqueeze(img, dim=0)
+
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file {} does not exist.".format(json_path)
+    with open(json_path, 'r') as f:
+        class_indict = json.load(f)
+
+    model = MobileNetV2(num_classes=5).to(device)
+    model_weight_path = "./mobilenet_v2_self.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+        print_res = "predicted class is: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                                  predict[predict_cla].numpy())
+
+        plt.title(print_res)
+
+        for i in range(len(predict)):
+            print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                      predict[i].numpy()))
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
